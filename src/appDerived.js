3e0de8e31ce39fb5c613f2da5b393e9381106118
Dmitry <dmitry.litvinov.spb@gmail.com>
2019-08-13 19:06:16 +0300

    add derived input

---
+++ src/appDerived.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import Input from './input'
+import Lazy from './lazy'
+import Derived from './derived'
+
+export default function() {
+	const [min, setMin] = useState(10)
+
+	setTimeout(() => setMin(30), 1000)
+
+	return (
+		<div>
+			<h2>Input counter</h2>
+			<p><i><b>Issue</b>: input can't be changed using React's `onChange` event</i></p>
+			<Input min={10} max={20} />
+
+			<h2>Input lazy counter</h2>
+			<p><i><b>Solution:</b> use staging value and change it `onBlur`</i></p>
+			<Lazy min={10} max={20} />
+
+			<hr/>
+
+			<h2>Input derived counter</h2>
+			<Derived min={min} max={20} key={`${min}:50`} />
+		</div>
+	)
+}
---
+++ src/derived.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Input extends React.Component {
+	static propTypes = {
+		min: PropTypes.number,
+		max: PropTypes.number
+	}
+
+	/*
+	* !antipattern
+	* set state from props
+	* better use `key` for rerender
+	static getDerivedStateFromProps(props, state) {
+		console.log(props)
+		state.count = Math.min(Math.max(state.count, props.min), props.max)
+		state.inputValue = state.count
+		return state
+	}*/
+
+	state = {
+		count: this.props.min,
+		inputValue: this.props.min,
+		min: this.props.min,
+		max: this.props.max
+	}
+
+	setCounter(number) {
+		const count = Math.min(Math.max(number, this.props.min), this.props.max)
+		this.setState({count, inputValue: count})
+	}
+
+	decrease = () => {
+		this.setCounter(this.state.count - 1)
+	}
+
+	increase = () => {
+		this.setCounter(this.state.count + 1)
+	}
+
+	handleChange(value) {
+		this.setState({ inputValue: value })
+	}
+
+	fixCounter = () => {
+		const cnt = parseInt(this.state.inputValue)
+		this.setCounter(isNaN(cnt) ? this.props.min : cnt)
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.decrease}>-</button>
+				<input 
+					type="text" 
+					value={this.state.inputValue}
+					onChange={(e) => this.handleChange(e.target.value)}
+					onBlur={this.fixCounter}
+				/>
+				<button onClick={this.increase}>+</button>
+				<span> <b>min</b>: {this.props.min} | max: {this.state.max}</span>
+			</div>
+		)
+	}
+}
+
+Input.defaultProps = {
+	min: 0,
+	max: 10
+}
