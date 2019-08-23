3170d27bfee423e71daaf0697e0fe39bc0439b15
Dmitry <dmitry@local.com>
2019-08-23 20:17:12 +0300

    add mobx order

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,12 +1,64 @@
 import React from 'react'
 import {Form, Button, Modal} from 'react-bootstrap'
 import PropTypes from 'prop-types'
+import orderModel from '@s/order'
+import router from '@s/router'
+import {CART, RESULT} from '@/consts'
+import {observer} from 'mobx-react'
+import cartModel from '@s/cart'
 
-export default class extends React.Component {
+export default @observer class extends React.Component {
+	state = {
+		showModal: false
+	}
+
+	show = (status) => {
+		this.setState({showModal: status})
+	}
+
+	confirm = () => {
+		this.show(false)
+		router.moveTo(RESULT)
+	}
+
 	render() {
+		const formFields = []
+		for (const name in orderModel.formData) {
+			const field = orderModel.formData[name]
+			formFields.push(
+				<Form.Group key={name}>
+					<Form.Label>{field.label}</Form.Label>
+					<Form.Control
+						type="text"
+						value={field.value}
+						onChange={(e) => orderModel.change(name, e.target.value)}
+						/>
+					{field.valid === false && <Form.Text>{field.errorText}</Form.Text>}
+				</Form.Group>
+			)
+		}
+
 		return (
 			<div>
 				<h2>Order</h2>
+				<Form>{formFields}</Form>
+				<Button variant="warning" onClick={() => router.moveTo(CART)}>Back to cart</Button>
+				&nbsp;
+				<Button variant="primary" onClick={() => this.show(true)} disabled={!orderModel.disabled}>Apply order</Button>
+
+				<Modal show={this.state.showModal} backdrop="static" onHide={() => this.show(false)}>
+					<Modal.Header closeButton>
+						<Modal.Title>Check info</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<p><strong>Total price: {cartModel.total}</strong></p>
+						<p>Is everything correct?</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={() => this.show(false)}>Cancel</Button>
+						<Button variant="primary" onClick={this.confirm}>Yes, Submit</Button>
+					</Modal.Footer>
+				</Modal>
 			</div>
 		)
 	}
---
+++ src/store/order.js
@@ -0,0 +1,40 @@
+import {observable, computed, action} from 'mobx'
+
+class Order {
+	@observable formData = {
+		name: {
+			value: '',
+			label: 'Name',
+			validator: val => /^[a-zA-Z]{2,}$/.test(val),
+			errorText: 'Only latin symbols. Min 3',
+			valid: null
+		},
+		email: {
+			value: '',
+			label: 'Email',
+			validator: val => /^.+@.+$/.test(val),
+			errorText: 'Email should contain `@` symbol',
+			valid: null
+		},
+		phone: {
+			value: '',
+			label: 'Phone',
+			validator: val => /^[0-9]{7,15}$/.test(val),
+			errorText: 'Only numbers. Min 7',
+			valid: null
+		},
+	}
+
+	@computed get disabled() {
+		return Object.values(this.formData).every(input => input.valid)
+	}
+
+	@action change(key, value) {
+		const field = this.formData[key]
+		field.value = value
+		field.valid = field.validator(field.value)
+	}
+
+}
+
+export default new Order
