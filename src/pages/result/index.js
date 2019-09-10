b9cf6da5e04374815c692255d4a5377369ebaeca
Dmitry <dmitry@local.com>
2019-09-10 20:16:45 +0300

    add news & url builder helper

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import cartModel from '@s/cart'
 import orderModel from '@s/order'
+import {urlBuilder} from '@/routes'
+import {Link} from 'react-router-dom'
+
 
 export default class extends React.Component {
 	render() {
+		const temp_posts = [1, 2, 3]
+		const newsLinks = temp_posts.map(id => {
+			return <div key={id}><Link to={urlBuilder('news', {id})}>Post {id}</Link></div>
+		})
+
 		return (
 			<div>
 				<h2>Success</h2>
 				<p>Thank you, {orderModel.data.name} for your order!</p>
 				<p>Your total price: {cartModel.total}</p>
+
+				<h4>News</h4>
+				{newsLinks}
 			</div>
 		)
 	}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import Cart from '@p/cart'
 import Order from '@p/order'
 import Result from '@p/result'
+import Post from '@p/post'
 import Error404 from '@c/errors/404'
 
 export const routes = [
 	{ name: 'home', path: '/', component: Cart, exact: true },
 	{ name: 'order', path: '/order', component: Order, exact: true },
 	{ name: 'success', path: '/success', component: Result, exact: true },
+	{ name: 'news', path: '/news/:id', component: Post, exact: true },
 	{ name: '404', path: '**', component: Error404, exact: true }
 ]
 
@@ -17,3 +19,15 @@ routes.forEach(route => {
 		routesMap[route.name] = route.path	
 	}
 })
+
+export const urlBuilder = (name, params) => {
+	if(!routesMap.hasOwnProperty(name)) return null
+
+	let url = routesMap[name]
+
+	for(let key in params) {
+		url = url.replace(':' + key, params[key])
+	}
+
+	return url
+}
