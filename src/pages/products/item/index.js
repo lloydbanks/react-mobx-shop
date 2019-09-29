c7aad43a64b2d5fb06492cef9f20b4b977d088ca
lloydbanks <100promail@gmail.com>
2019-09-29 14:45:58 +0300

    refactor product into present.component

--- src/pages/products/item/index.js
+++ src/pages/products/item/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import E404 from '@c/errors/404'
 import {Link} from 'react-router-dom'
 import {routesMap} from '@/routes'
 import {inject} from 'mobx-react'
+import ProductItem from '@c/products/item'
 
 export default inject('stores') (function(props) {
 	const {id} = props.match.params
 	const {products} = props.stores
 	const product = products.getById(id)
 
-	return (
-		<div>
-			<h2>{product.title}</h2>
-			<p>Price: {product.price}</p>
-			<Link to={routesMap.home} className="btn btn-warning">Back</Link>
-		</div>
-	)
+	if(product === null) {
+		return <E404 />
+	} else {
+		return <ProductItem 
+			title={product.title}
+			price={product.price}
+			backUrl={routesMap.home}
+			Link={Link}
+		/>
+	}
 })
