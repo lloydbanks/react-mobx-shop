e7856a924961bf56a07dadf95fb9c39258caea5b
Dmitry <dmitry@local.com>
2019-10-01 16:36:28 +0300

    add product 404

--- src/pages/products/item/index.js
+++ src/pages/products/item/index.js
@@ -2,17 +2,22 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {routesMap} from '@/routes'
 import {inject} from 'mobx-react'
+import E404 from '@p/error404'
 
 export default inject('stores') (function(props) {
 	const {id} = props.match.params
 	const {products} = props.stores
 	const product = products.getById(id)
 
-	return (
-		<div>
-			<h2>{product.title}</h2>
-			<p>Price: {product.price}</p>
-			<Link to={routesMap.home} className="btn btn-warning">Back</Link>
-		</div>
-	)
+	if(product) {
+		return (
+			<div>
+				<h2>{product.title}</h2>
+				<p>Price: {product.price}</p>
+				<Link to={routesMap.home} className="btn btn-warning">Back</Link>
+			</div>
+		)
+	} else {
+		return <E404 />
+	}
 })
