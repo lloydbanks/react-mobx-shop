df16abc7e18e3c6e18302820046c6f08bb693091
lloydbanks <100promail@gmail.com>
2019-10-06 16:05:50 +0300

    move Provider to main

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {CART, ORDER, RESULT} from './consts'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {observer, Provider} from 'mobx-react'
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
 import {routes, routesMap} from '@/routes'
-import stores from '@s'
+import store from '@/hocs/store'
 
-export default @observer class extends React.Component {
+class App extends React.Component {
 	render() {
 		const Routes = routes.map(route => {
 			const {path, component, exact} = route
@@ -14,33 +13,33 @@ export default @observer class extends React.Component {
 			return <Route path={path} component={component} exact={exact} key={path} />
 		})
 		return (
-			<Provider stores={stores}>
-				<BrowserRouter>
-					<div className="container">
-						<div className="row">
-							<div className="col col-3">
-								<h2>Sidebar</h2>
-								<ul className="list-group">
-									<li className="list-group-item">
-										<Link to={routesMap.home}>Home</Link>
-									</li>
-									<li className="list-group-item">
-										<Link to={routesMap.cart}>Cart</Link>
-									</li>
-									<li className="list-group-item">
-										<Link to={routesMap.order}>Order</Link>
-									</li>
-								</ul>
-							</div>
-							<div className="col col-9">
-								<Switch>
-									{Routes}
-								</Switch>
-							</div>
+			<BrowserRouter>
+				<div className="container">
+					<div className="row">
+						<div className="col col-3">
+							<h2>Sidebar</h2>
+							<ul className="list-group">
+								<li className="list-group-item">
+									<Link to={routesMap.home}>Home</Link>
+								</li>
+								<li className="list-group-item">
+									<Link to={routesMap.cart}>Cart</Link>
+								</li>
+								<li className="list-group-item">
+									<Link to={routesMap.order}>Order</Link>
+								</li>
+							</ul>
+						</div>
+						<div className="col col-9">
+							<Switch>
+								{Routes}
+							</Switch>
 						</div>
 					</div>
-				</BrowserRouter>
-			</Provider>
+				</div>
+			</BrowserRouter>
 		)
 	}
 }
+
+export default store(App)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './app'
+import {Provider} from 'mobx-react'
+import stores from '@s'
 
-ReactDom.render(<App/>, document.getElementById('app'))
+ReactDom.render(<Provider stores={stores}><App/></Provider>, document.getElementById('app'))
